4258ad5b9cd34bac20c7f7a63f2a1d3a0aafc9bf
MahmoudAbojahel <mahmoudabujahel@gmail.com>
2022-08-14 16:16:53 +0300

    Done Sample

---
+++ OpenXmlSample/OpenXmlSample/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpenXmlSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXmlSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+ 
+         
+        public async Task<IActionResult> ExportTemplates()
+        {  
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "templates", "templates.docx");
+
+            byte[] byteArray = System.IO.File.ReadAllBytes(path);
+
+            string exportName = $"Export_{Guid.NewGuid()}.docx";
+            string newPath = Path.Combine(Directory.GetCurrentDirectory(), "Exports", exportName);
+
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(byteArray, 0, byteArray.Length); 
+                using (WordprocessingDocument doc = WordprocessingDocument.Open(stream, true))
+                {
+                    doc.ChangeDocumentType(DocumentFormat.OpenXml.WordprocessingDocumentType.Document);
+                    doc.InsertText("Test", "MahmoudAhmadAbujahel");
+                }
+                using (FileStream fs = new FileStream(newPath, FileMode.Create))
+                { 
+                    stream.WriteTo(fs); 
+                }
+            }
+              
+            return File(System.IO.File.ReadAllBytes(newPath), "application/vnd.openxmlformats-officedocument.wordprocessingml.document"); 
+
+        }
+
+
+    }
+}
---
+++ OpenXmlSample/OpenXmlSample/Models/OpenxmlClass.cs
@@ -0,0 +1,57 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ 
+    public static class OpenxmlClass
+    {
+        public static WordprocessingDocument InsertText(this WordprocessingDocument doc, string contentControlTag, string text)
+        {
+            SdtElement element = doc.MainDocumentPart.Document.Body.Descendants<SdtElement>()
+              .FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == contentControlTag);
+
+            if (element == null)
+                throw new ArgumentException($"ContentControlTag \"{contentControlTag}\" doesn't exist.");
+
+            element.Descendants<Text>().First().Text = text;
+            element.Descendants<Text>().Skip(1).ToList().ForEach(t => t.Remove()); 
+            return doc;
+        }
+
+
+    internal static WordprocessingDocument RemoveSdtBlocks(this WordprocessingDocument doc, IEnumerable<string> contentBlocks)
+    {
+        List<SdtElement> SdtBlocks = doc.MainDocumentPart.Document.Descendants<SdtElement>().ToList();
+
+        if (contentBlocks == null)
+            return doc;
+
+        foreach (var s in contentBlocks)
+        {
+            SdtElement currentElement = SdtBlocks.FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == s);
+            if (currentElement == null)
+                continue;
+            IEnumerable<OpenXmlElement> elements = null;
+
+            if (currentElement is SdtBlock)
+                elements = (currentElement as SdtBlock).SdtContentBlock.Elements();
+            else if (currentElement is SdtCell)
+                elements = (currentElement as SdtCell).SdtContentCell.Elements();
+            else if (currentElement is SdtRun)
+                elements = (currentElement as SdtRun).SdtContentRun.Elements();
+
+            foreach (var el in elements)
+                currentElement.InsertBeforeSelf(el.CloneNode(true));
+            currentElement.Remove();
+        }
+        return doc;
+    }
+
+
+
+
+}
